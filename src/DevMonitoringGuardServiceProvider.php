acc90e958628b9f6dea3322095fca7165b783e69
ikeogu <ikeogu322@gmail.com>
2025-09-23 10:51:14 +0100

    chores: added DB seeder

--- src/DevMonitoringGuardServiceProvider.php
+++ src/DevMonitoringGuardServiceProvider.php
@@ -4,6 +4,8 @@ namespace Emmanuelikeogu\DevMonitoringGuard;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\LogViewerServiceProvider as LogViewerLogViewerServiceProvider;
+
 
 class DevMonitoringGuardServiceProvider extends ServiceProvider
 {
@@ -32,19 +34,70 @@ class DevMonitoringGuardServiceProvider extends ServiceProvider
             __DIR__ . '/../stubs/tsconfig.json' => base_path('tsconfig.json'),
 
             // Migration
-            __DIR__ . '/../Database/migrations/create_dev_users_table.php.stub'
-                => database_path('migrations/' . date('Y_m_d_His') . '_create_dev_users_table.php'),
-            __DIR__ . '/../Database/Seeders/DevUserSeeder.php'
-                => database_path('seeders/DevUserSeeder.php'),
+            __DIR__ . '/../database/migrations/create_dev_users_table.php.stub'
+            => database_path('migrations/' . date('Y_m_d_His') . '_create_dev_users_table.php'),
+            __DIR__ . '/../database/Seeders/DevUserSeeder.php'
+            => database_path('seeders/DevUserSeeder.php'),
 
             // Vendor configs
             base_path('vendor/opcodesio/log-viewer/config/log-viewer.php') => config_path('log-viewer.php'),
             base_path('vendor/guanguans/laravel-scramble/config/scramble.php') => config_path('scramble.php'),
             base_path('vendor/laravel/telescope/config/telescope.php') => config_path('telescope.php'),
-
             // Package config
             __DIR__ . '/../config/devguard.php' => config_path('devguard.php'),
         ], 'dev-guard-all');
+
+        $this->publishVendorConfigs();
+    }
+
+    protected function publishVendorConfigs()
+    {
+        $vendorConfigs = [];
+
+        // Check for Log Viewer
+        if (class_exists(LogViewerLogViewerServiceProvider::class)) {
+            $logViewerConfig = base_path('vendor/opcodesio/log-viewer/config/log-viewer.php');
+            if (file_exists($logViewerConfig)) {
+                $vendorConfigs[$logViewerConfig] = config_path('log-viewer.php');
+            }
+        }
+
+        // Check for Scramble
+        if (class_exists(\Dedoc\Scramble\ScrambleServiceProvider::class)) {
+            $scrambleConfig = base_path('vendor/dedoc/scramble/config/scramble.php');
+            if (file_exists($scrambleConfig)) {
+                $vendorConfigs[$scrambleConfig] = config_path('scramble.php');
+            }
+        }
+
+        // Check for Telescope
+        if (class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $telescopeConfig = base_path('vendor/laravel/telescope/config/telescope.php');
+            if (file_exists($telescopeConfig)) {
+                $vendorConfigs[$telescopeConfig] = config_path('telescope.php');
+            }
+        }
+
+        if (!empty($vendorConfigs)) {
+            $this->publishes($vendorConfigs, 'dev-guard-vendor-configs');
+            $this->publishes($vendorConfigs, 'dev-guard-all');
+        }
+    }
+
+
+    protected function registerVendorProviders()
+    {
+        $providers = [
+            LogViewerLogViewerServiceProvider::class,
+            \Dedoc\Scramble\ScrambleServiceProvider::class,
+            \Laravel\Telescope\TelescopeServiceProvider::class,
+        ];
+
+        foreach ($providers as $provider) {
+            if (class_exists($provider)) {
+                $this->app->register($provider);
+            }
+        }
     }
 
     public function register()
@@ -54,5 +107,7 @@ class DevMonitoringGuardServiceProvider extends ServiceProvider
             __DIR__ . '/../config/devguard.php',
             'devguard'
         );
+
+        $this->registerVendorProviders();
     }
 }
